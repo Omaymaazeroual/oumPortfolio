12f4815925de66be8d1526ad9061b1bc5c12ba0f
oum <oumeima.azeroual@gmail.com>
2023-11-10 09:14:43 +0100

    third commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
+import menu from "./menu.png";
+import close from "./close.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
